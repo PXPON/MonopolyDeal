b7c98ee23593c9283ce719505adf4eae00f6eb60
Paxton Perry <paxton.perry@protonmail.com>
2024-12-08 03:56:00 -0800

    Added code for the Money class

--- money.py
+++ money.py
@@ -1,4 +1,8 @@
 # This class defines a Money card
 # The bills come in $1, $2, $3, $4, $5 and $10 denominations
+from card import Card
 
-
+class MoneyCard(Card):
+	def __init__(self, denomination):
+		# Initialize a money card	
+		self.denomination = denomination
